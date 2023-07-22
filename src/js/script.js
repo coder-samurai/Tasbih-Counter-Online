a621169313389feaad31ce82692afbacc17aa528
coder-samurai <aelboutaheri@gmail.com>
2023-07-22 09:36:04 +0200

    b

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,24 @@
-var AddButton = document.getElementsByClassName("AddButton");
+var AddButton = document.getElementById("AddButton");
+var MinButton = document.getElementById("MinButton");
+var Counter = 0;
 
 
-function Add() {
-    AddButton.innerText += 1;
-    alert(AddButton.innerText)
-}
+AddButton.addEventListener("click", function() {
+    Counter += 99;
+    if (Counter >= 999) {
+        alert("This is the max, please reset the button.");
+        AddButton.innerHTML = 999;
+        Counter = 999;
+    } else {
+        AddButton.innerHTML = Counter;
+    }
+});
+
+MinButton.addEventListener("click", function() {
+    Counter -= 1;
+    if (Counter < 0) {
+        Counter = 0;
+    } else {
+        AddButton.innerHTML = Counter;
+    }
+});
